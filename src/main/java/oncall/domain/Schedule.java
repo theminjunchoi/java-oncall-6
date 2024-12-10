3a7fb69ae2f64ccfa23b703abc52bb0ff1016808
MinJun Choi <mj043000@naver.com>
2024-12-10 20:18:37 +0900

    feat: schedule에 월 관련 변수 추가

--- src/main/java/oncall/domain/Schedule.java
+++ src/main/java/oncall/domain/Schedule.java
@@ -1,11 +1,13 @@
 package oncall.domain;
 
 public class Schedule {
+    private final int month;
     private final int date;
     private final Worker worker;
     private final Day day;
 
-    public Schedule(int date, Worker worker, Day day) {
+    public Schedule(int month, int date, Worker worker, Day day) {
+        this.month = month;
         this.date = date;
         this.worker = worker;
         this.day = day;
